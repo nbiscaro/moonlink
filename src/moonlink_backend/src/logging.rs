ef11c90da12f7ec48a9643f2b6942e71a130b06f
Nolan Biscaro <biscaronolan@gmail.com>
2025-06-17 19:55:55 +0000

    avoid print logs on test

--- src/moonlink_backend/src/logging.rs
+++ src/moonlink_backend/src/logging.rs
@@ -1,18 +1,22 @@
+use tracing_subscriber::Layer;
+
 pub fn init_logging() {
-    use std::io;
     use tracing_subscriber::{
         fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry,
     };
 
-    // base registry
-    let registry = Registry::default()
-        .with(
-            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")), // fallback
-        )
-        .with(fmt::layer().with_writer(io::stderr).with_ansi(false));
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let fmt_layer = fmt::layer()
+        .with_test_writer()
+        .with_ansi(false)
+        .with_filter(env_filter);
+
+    // Compose the subscriber.
+    let subscriber = Registry::default().with(fmt_layer);
 
     #[cfg(feature = "profiling")]
-    let registry = registry.with(console_subscriber::spawn());
+    let subscriber = subscriber.with(console_subscriber::spawn());
 
-    let _ = registry.try_init();
+    let _ = subscriber.try_init();
 }
